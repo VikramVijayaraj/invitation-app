592236e8c512a7b1b70595fb9f0b845f84c15ba0
Vikram Vijayaraj <vikramvijayaraj@Vikrams-MacBook-Pro.local>
2025-04-09 20:13:37 +0530

    temp push for pdf extract

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import WeddingSection from "./components/WeddingSection";
 import { Separator } from "@/components/ui/separator";
 
 function App() {
-  const [started, setStarted] = useState(false);
+  const [started, setStarted] = useState(true); // should be false by default
 
   function handleStartInvitation() {
     const audio = new Audio("/music/bgm.mp3");
